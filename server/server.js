821f320f531147fde536aceb5f1ad4d65aaa4b37
Dan Griffith <dan.griffith@tierpoint.com>
2018-03-30 12:41:29 -0500

    added socket

--- server/server.js
+++ server/server.js
@@ -1,13 +1,24 @@
 const path = require('path');
 const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
 const publicPath = path.join(__dirname, '/../public');
 const port = process.env.PORT || 3000
+
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
 app.use(express.static(publicPath));
 
-app.get('/', (req, res) => {
-    res.status(200).send(`Started on Port ${port}`); 
- });
- 
- app.listen(port, () => console.log(`Started on Port ${port}`));
+io.on('connection', (socket) => {
+    console.log('New user connected');
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
+
+
+server.listen(port, () => console.log(`Started on Port ${port}`));
 
