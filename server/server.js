a3b8b28276ca0808b4ffed73df002786e5683dbe
Dan Griffith <dan.griffith@tierpoint.com>
2018-04-03 13:45:17 -0500

    add form and show messages

--- server/server.js
+++ server/server.js
@@ -15,11 +15,12 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
-    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the cat'));
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat'));
     socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined the chat')) 
 
-    socket.on('createMessage', (message) => {        
-        socket.broadcast.emit('newMessage', generateMessage(message.from, message.text))
+    socket.on('createMessage', (message, callback) => {        
+        io.emit('newMessage', generateMessage(message.from, message.text))
+        callback('This is from the server.');
     });
 
     socket.on('disconnect', () => {
